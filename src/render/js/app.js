95c35965bcfad7fc5fdeac47706d46acd8f782e9
zqbright <502268502@qq.com>
2019-01-08 22:45:55 +0800

    study react

--- src/render/js/app.js
+++ src/render/js/app.js
@@ -1,4 +1,32 @@
-import React from 'react';
-import ReactDom from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDom.render(<h1>Hello, world!</h1>, document.getElementById("app"));
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello , World</h1>
+        <h1>It is {this.state.date.toLocaleTimeString()}</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Clock />, document.getElementById("app"));
